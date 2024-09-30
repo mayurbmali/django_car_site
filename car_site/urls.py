f861648a3ee8890a34bf1a8fe18b93b7a27961a5
Mayur Mali <mayurbmali80@gmail.com>
2024-09-30 22:07:16 +0530

    updating files

--- car_site/urls.py
+++ car_site/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('<int:pk>/', car_detail, name='car_detail'),
     path('', car_list, name='car_list'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
